3f7a6c32ff3b6d1507f8ec42e5349bbea96d098c
James Mu <jamesmu365@gmail.com>
2023-10-17 18:58:37 -0700

    Initial replay_game class

---
+++ src/replay_game/replay_game.py
@@ -0,0 +1,5 @@
+class replayGame:
+    def __init__(self, yt_video_url, date_created, name):
+        self._yt_video_url = yt_video_url
+        self._date_create = date_created
+        self._name = name
