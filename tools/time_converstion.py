d8bb2d19d99b373bef97414a296e6f5f40852b3f
James Mu <jamesmu365@gmail.com>
2023-11-06 08:35:18 -0800

    Implemented time_conversion

---
+++ tools/time_converstion.py
@@ -0,0 +1,5 @@
+def convertMilis(millis: int) -> tuple(int, int, int):
+    seconds = (millis / 1000) % 60
+    minutes = (millis / (1000 * 60)) % 60
+    hours = (millis / (1000 * 60 * 60)) % 24
+    return (hours, minutes, seconds)
