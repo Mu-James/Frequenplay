d2199926ad343ef6236616280c07736c43879655
James Mu <jamesmu365@gmail.com>
2023-11-05 19:15:48 -0800

    Restructuring

--- src/replay_game/frequenplay_game.py
+++ src/replay_game/frequenplay_game.py
@@ -1,5 +1,5 @@
-class replayGame:
+class frequenplayGame:
     def __init__(self, yt_video_url, date_created, name):
         self._yt_video_url = yt_video_url
-        self._date_create = date_created
+        self._date_created = date_created
         self._name = name
