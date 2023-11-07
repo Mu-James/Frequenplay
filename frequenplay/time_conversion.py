76604fbc657eeb2123a9f3f03e69f0bf52191fc6
James Mu <jamesmu365@gmail.com>
2023-11-06 18:31:16 -0800

    Renamed and reimplemented convertMilisTimestamp

--- frequenplay/time_conversion.py
+++ frequenplay/time_conversion.py
@@ -1,5 +1,5 @@
-def convertMilis(millis: int) -> tuple[int, int, int]:
-    seconds = (millis / 1000) % 60
-    minutes = (millis / (1000 * 60)) % 60
-    hours = (millis / (1000 * 60 * 60)) % 24
-    return (hours, minutes, seconds)
+from datetime import timedelta
+
+def convertMilisTimestamp(ms: int) -> timedelta:
+    timestamp = timedelta(milliseconds=ms)
+    return timestamp
