0be549ad4a6a5cf755c8bac2fe41a982bda2f188
James Mu <jamesmu365@gmail.com>
2023-12-16 11:06:13 -0800

    Name change

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ sys.path.append(os.path.abspath("frequenplay"))
 from frequenplay_game import frequenplayGame
 
 if __name__ == "__main__":
-    fg1 = frequenplayGame("https://www.youtube.com/watch?v=oL5XNGhelGI", "11/05/2023", "Troll")
-    fg1.print_time()
+    fg_test = frequenplayGame("https://www.youtube.com/watch?v=oL5XNGhelGI", "11/05/2023", "Troll")
+    fg_test.print_time()
