5914f5622ec4ed2f28e641d7bc037816dd5f1601
James Mu <jamesmu365@gmail.com>
2023-12-18 10:56:10 -0800

    frequenplayGameMC test

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys, os
 sys.path.append(os.path.abspath("frequenplay"))
 
-from frequenplay_game import frequenplayGame
+import frequenplay_game as fg
 
 if __name__ == "__main__":
-    fg_test = frequenplayGame("https://www.youtube.com/watch?v=oL5XNGhelGI", "11/05/2023", "Troll")
+    fg_test = fg.frequenplayGameMC("https://www.youtube.com/watch?v=oL5XNGhelGI", "11/05/2023", "Troll")
+    fg_test.generate_random_answer_bank(5)
+    fg_test._print_answer_bank()
