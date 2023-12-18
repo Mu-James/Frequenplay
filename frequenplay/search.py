7d3e88ea12e5c59286cb00a3f5aeeb96b604abaa
James Mu <jamesmu365@gmail.com>
2023-12-18 08:38:10 -0800

    Edited for multiple most replayed timestamps

--- frequenplay/search.py
+++ frequenplay/search.py
@@ -1,13 +1,14 @@
 from time_conversion import convertMilisTimestamp
 
-def mostReplayedSearch(timestamp_intesnties: list[dict]) -> tuple[int, int, int]:
+def mostReplayedSearch(timestamp_intesnties: list[dict]) -> list:
     """
-    Returns a timedelta object representing the most replayed
+    Returns a list of timedelta objects representing the most replayed
     timestamp given.
     """
-    
+    most_replayed = []
+
     for timestamp in timestamp_intesnties:
         if timestamp["intensityScoreNormalized"] == 1:
-            return convertMilisTimestamp(timestamp["startMillis"])
-    else:
-        return None
+            most_replayed.append(convertMilisTimestamp(timestamp["startMillis"]))
+
+    return most_replayed
