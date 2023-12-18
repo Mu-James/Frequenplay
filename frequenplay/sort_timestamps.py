8af2b9d418610b993eae25db846a82fe08e82734
James Mu <jamesmu365@gmail.com>
2023-12-18 09:37:04 -0800

    Counts the number of entries

--- frequenplay/sort_timestamps.py
+++ frequenplay/sort_timestamps.py
@@ -1,18 +1,22 @@
 from time_conversion import convertMilisTimestamp
 
-def sortTimestamps(timestamp_intesnties: list[dict]) -> tuple(list, list):
+def sortTimestamps(timestamp_intesnties: list[dict]) -> tuple(tuple(list, int), tuple(list, int)):
     """
-    Returns a tuple of list of timedelta objects.
+    Returns a tuple of two tuples each with a list of timedelta objects and the number of entries.
     The first list contains most replayed timestamps. A timestamps is the most replayed if it has a normalized intensity score equal to 1.
     The second list contains  other timestamps that have a normalized intensity score less than 1.
     """
     most_replayed = []
+    mr_entries = 0
     other = []
+    o_entries = 0
 
     for timestamp in timestamp_intesnties:
         if timestamp["intensityScoreNormalized"] == 1:
             most_replayed.append(convertMilisTimestamp(timestamp["startMillis"]))
+            mr_entries += 1
         else:
             other.append(convertMilisTimestamp(timestamp["startMillis"]))
+            o_entries += 1
 
-    return (most_replayed, other)
+    return ((most_replayed, mr_entries), (other, o_entries))
