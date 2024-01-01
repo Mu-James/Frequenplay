82d39cd8bc12157c435962d06cd3898ceae0c83e
James Mu <jamesmu365@gmail.com>
2023-12-31 18:54:19 -0800

    Incomplete index function

--- core/frequenplay/views.py
+++ core/frequenplay/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import MultipleChoiceGame
 
 # Create your views here.
+def index(request):
+    """View function for home page of site"""
+    num_games = MultipleChoiceGame.objects.all().count()
+
+    context = {
+        "num_games" : num_games
+    }
+
+    return render(request, 'index.html', context=context)
