8348d740ce710b72471db55664ccd3057967ee36
James Mu <jamesmu365@gmail.com>
2024-01-03 11:02:39 -0800

    Adjusted regex for ids in urls

--- core/frequenplay/urls.py
+++ core/frequenplay/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path("<game_id>/", views.game_read, name="read"),
-    path("<int:game_id>/results", views.game_results, name="results"),
-    path("<int:game_id>/play", views.game_play, name="play")
+    path("<game_id>/results", views.game_results, name="results"),
+    path("<game_id>/play", views.game_play, name="play")
 ]
