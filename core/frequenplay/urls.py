842179a150595ff6b0462c1d9057abb76babc7be
James Mu <jamesmu365@gmail.com>
2024-01-04 11:35:18 -0800

    Added namespace

--- core/frequenplay/urls.py
+++ core/frequenplay/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "frequenplay"
 urlpatterns = [
     path('', views.index, name = 'index'),
     path("<game_id>/", views.game_read, name="read"),
